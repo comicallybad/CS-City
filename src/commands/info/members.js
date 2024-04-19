2a1c89ac8e0b9bbf3962cf2cd8cf47717a0c804a
ComicallyBad <amp5646@yahoo.com>
2024-04-19 13:30:52 -0400

    Added members command

---
+++ src/commands/info/members.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { r } = require("../../../utils/functions/functions.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('members')
+        .setDescription('Displays the guild member count.'),
+    execute: (interaction) => {
+        const embed = new EmbedBuilder()
+            .setTitle(`**Members**`)
+            .setColor("#0efefe")
+            .setDescription(`${interaction.guild.memberCount}`)
+            .setTimestamp()
+
+        return r(interaction, "", embed);
+    }
+}
