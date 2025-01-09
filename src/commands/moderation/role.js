ed2961fde9aa96ed28e2e553346806ac719a3602
ComicallyBad <amp5646@yahoo.com>
2025-01-08 22:59:12 -0500

    allow role icons

--- src/commands/moderation/role.js
+++ src/commands/moderation/role.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, AuditLogEvent } = require("discord.js");
-const { s, re, delr } = require("../../../utils/functions/functions.js");
+const { s, re, er, delr } = require("../../../utils/functions/functions.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,18 +7,20 @@ module.exports = {
         .setDescription("SERVER BOOSTERS - Create your own role.")
         .addStringOption(option => option.setName("name").setDescription("The name of the role.").setMinLength(1).setMaxLength(64).setRequired(true))
         .addStringOption(option => option.setName("color").setDescription("The hex color of the role.").setRequired(true))
-        .addStringOption(option => option.setName("emoji").setDescription("The unicode emoji of the role.")),
+        .addStringOption(option => option.setName("icon").setDescription("The emoji/icon url of the role.")),
     execute: async (interaction) => {
         if (!interaction.member.permissions.has(PermissionFlagsBits.ManageRoles) && !interaction.member.roles.cache.find(role => role.name.includes("Server Booster")))
             return re(interaction, "You don't have permission to manage roles.").then(() => delr(interaction, 15000));
 
         const name = interaction.options.getString("name");
         const color = interaction.options.getString("color");
-        const emoji = interaction.options.getString("emoji") || undefined;
+        const icon = interaction.options.getString("icon") || undefined;
         const logChannel = interaction.guild.channels.cache.find(channel => channel.name.includes("role-logs"));
 
         if (!color.match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/))
             return re(interaction, "Invalid hex color.").then(() => delr(interaction, 15000));
+
+        await re(interaction, "Creating your role, please wait...");
 
         try {
             const role = await interaction.guild.roles.create({
@@ -35,7 +37,29 @@ module.exports = {
                 return re(interaction, "An error occurred while creating the role.").then(() => delr(interaction, 15000));
 
             try {
-                if (emoji) await role.setUnicodeEmoji(emoji);
+                if (icon) {
+                    const iconRegex = /<a?:\w+:(\d+)>/;
+                    const urlRegex = /(https?:\/\/.*\.(?:png|jpg|gif))/i;
+                    const match = icon.match(iconRegex);
+                    const urlMatch = icon.match(urlRegex);
+                    const iconId = match ? match[1] : icon;
+
+                    if (urlMatch) {
+                        await role.setIcon(urlMatch[0]);
+                    } else {
+                        const fetchEmoji = interaction.guild.emojis.cache.find(e => e.name === icon || e.id === iconId);
+
+                        if (fetchEmoji) {
+                            if (!fetchEmoji.animated) {
+                                await role.setIcon(`https://cdn.discordapp.com/emojis/${fetchEmoji.id}.png`);
+                            } else {
+                                await role.setIcon(`https://cdn.discordapp.com/emojis/${fetchEmoji.id}.gif`);
+                            }
+                        } else {
+                            throw new Error("Invalid emoji or icon URL");
+                        }
+                    }
+                }
 
                 const fetchedLogs = await interaction.guild.fetchAuditLogs({
                     type: AuditLogEvent.RoleCreate,
@@ -100,19 +124,19 @@ module.exports = {
                         value: `${color}`,
                         inline: true
                     }, {
-                        name: "__**Emoji**__",
-                        value: `${emoji || "None"}`,
+                        name: "__**Icon**__",
+                        value: `${icon || "None"}`,
                         inline: true
                     })
 
                 if (logChannel) s(logChannel, "", embed)
-                return re(interaction, `Role ${role} has been created.`).then(() => delr(interaction, 15000));
+                return er(interaction, `Role ${role} has been created.`).then(() => delr(interaction, 15000));
             } catch (err) {
                 await role.delete();
-                return re(interaction, `An error occurred while creating the role:\n\`${err}\``).then(() => delr(interaction, 15000));
+                return er(interaction, `An error occurred while creating the role:\n\`${err}\``).then(() => delr(interaction, 15000));
             }
         } catch (err) {
-            return re(interaction, `An error occurred while creating the role:\n\`${err}\``).then(() => delr(interaction, 15000));
+            return er(interaction, `An error occurred while creating the role:\n\`${err}\``).then(() => delr(interaction, 15000));
         }
     }
 }
